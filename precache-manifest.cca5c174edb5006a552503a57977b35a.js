2d175f5c59a661098a7151c0d56305ad6edaca51
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-06-07 23:16:57 +0200

    Updates

---
+++ precache-manifest.cca5c174edb5006a552503a57977b35a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5551e7e8290c87efebe1286d4054404a",
+    "url": "/mars-weather-app/index.html"
+  },
+  {
+    "revision": "784f411e84d77ef60d8b",
+    "url": "/mars-weather-app/static/css/main.87b9db52.chunk.css"
+  },
+  {
+    "revision": "9e2799f58fe574b0484b",
+    "url": "/mars-weather-app/static/js/2.01aca1ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mars-weather-app/static/js/2.01aca1ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784f411e84d77ef60d8b",
+    "url": "/mars-weather-app/static/js/main.15af0829.chunk.js"
+  },
+  {
+    "revision": "ae21e0f908115e6e7b29",
+    "url": "/mars-weather-app/static/js/runtime-main.1a9d699c.js"
+  }
+]);
